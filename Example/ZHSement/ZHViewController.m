8762ff9cb4e703c7fff2556a80c24c217acca478
caixiang <caix@mail.open.cn>
2017-11-10 13:24:04 +0800

    ‘添加列子’

--- Example/ZHSement/ZHViewController.m
+++ Example/ZHSement/ZHViewController.m
@@ -7,17 +7,54 @@
 //
 
 #import "ZHViewController.h"
+#import "ZHSegmentViewController.h"
+#import "UIView+ZHSegmentPostion.h"
 
 @interface ZHViewController ()
+
+@property (nonatomic, weak) ZHSegmentViewController *segmentViewController;
 
 @end
 
 @implementation ZHViewController
 
+- (ZHSegmentViewController *)segmentViewController{
+    if (!_segmentViewController) {
+        ZHSegmentViewController *segmentViewController = [[ZHSegmentViewController alloc] init];
+        [self addChildViewController:segmentViewController];
+        _segmentViewController = segmentViewController;
+    }
+    return _segmentViewController;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //设置frame
+    self.segmentViewController.segmentBar.frame = CGRectMake(0, 0, self.view.width, 50);
+    self.segmentViewController.view.frame = CGRectMake(0, self.segmentViewController.segmentBar.height, self.view.width, self.view.height - self.segmentViewController.segmentBar.height);
+    [self.view addSubview:self.segmentViewController.view];
+    [self.view addSubview:self.segmentViewController.segmentBar];
+    
+    //配置数据源
+    NSArray *items = @[@"标签1", @"标签2", @"标签3",@"标签标签4", @"标签5"];
+    UIViewController *vc1 = [UIViewController new];
+    vc1.view.backgroundColor = [UIColor colorWithRed:0.419 green:0.781 blue:0.977 alpha:1.000];
+    UIViewController *vc2 = [UIViewController new];
+    vc2.view.backgroundColor = [UIColor colorWithRed:0.957 green:0.557 blue:0.231 alpha:1.000];
+    UIViewController *vc3 = [UIViewController new];
+    vc3.view.backgroundColor = [UIColor colorWithWhite:0.729 alpha:1.000];
+    UIViewController *vc4 = [UIViewController new];
+    vc4.view.backgroundColor = [UIColor colorWithWhite:0.890 alpha:1.000];
+    UIViewController *vc5 = [UIViewController new];
+    vc5.view.backgroundColor = [UIColor colorWithRed:0.979 green:0.378 blue:0.328 alpha:1.000];
+    [self.segmentViewController setUpWithItems:items childViewControllers:@[vc1,vc2,vc3,vc4,vc5]];
+    
+    //通过ZHSegmentBarConfig改变self.segmentViewController.segmentBar UI
+    [self.segmentViewController.segmentBar updateWithConfig:^(ZHSegmentBarConfig *config) {
+       config.itemSelectColor = [UIColor yellowColor];
+    }];
 }
 
 - (void)didReceiveMemoryWarning
